9cefef57178463730c071823292919f97f081d05
thiago <trtg10@gmail.com>
2016-07-26 13:38:52 -0300

    Trabalhando as cores

--- js/navTopo.js
+++ js/navTopo.js
@@ -1,19 +1,24 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    =============NAV TOPO=============
+    Created on : 14/07/2016, 21:32:57
+    Author     : Thiago Gomes
+    ==================================
+*/
 
 var offset = $('#menu').offset().top;
-var $menuTopo = $('#menuTopo'); // guardar o elemento na memoria para melhorar performance
+// guardar o elemento na memoria para melhorar performance
+var $menuTopo = $('#menuTopo'); 
 var $menu = $('#menu');
 
 $(document).on('scroll', function () {
-    if (offset <= $(window).scrollTop()) {
-        $menuTopo.removeClass('invisible');
+    if (offset <= $(window).scrollTop())
+    {        
+        $menuTopo.removeClass('invisible');        
         $menu.addClass('invisible');
-    } else {
-        $menuTopo.addClass('invisible');
+    } 
+    else
+    {
+        $menuTopo.addClass('invisible');        
         $menu.removeClass('invisible');
     }
 });
